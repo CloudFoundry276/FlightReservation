b1ec146db9fb1c1ad4df8e1d5d8d2f6d4374d2f9
CloudFoundry276 <cloudfoundry276@gmail.com>
2022-03-19 19:52:37 +0530

    created display flights module

--- src/app/components/display-flights/display-flights.component.ts
+++ src/app/components/display-flights/display-flights.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ReservationService } from 'src/app/services/reservation.service';
 
 @Component({
   selector: 'app-display-flights',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DisplayFlightsComponent implements OnInit {
 
-  constructor() { }
+  data:any;
+
+  constructor(private reservationService:ReservationService, private router:Router) { }
 
   ngOnInit(): void {
+    this.data = this.reservationService.flightData;
+  }
+
+  public onSelect(id:number): any {
+    this.router.navigate(['/passengerDetails/'+id]);
   }
 
 }
