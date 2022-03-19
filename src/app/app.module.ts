dc0a79a9bce0115e09f0efd7dd993791bfca9a2d
CloudFoundry276 <cloudfoundry276@gmail.com>
2022-03-19 16:07:08 +0530

    components and services are setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FindFlightsComponent } from './components/find-flights/find-flights.component';
+import { DisplayFlightsComponent } from './components/display-flights/display-flights.component';
+import { PassengerDetailsComponent } from './components/passenger-details/passenger-details.component';
+import { ConfirmReservationComponent } from './components/confirm-reservation/confirm-reservation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FindFlightsComponent,
+    DisplayFlightsComponent,
+    PassengerDetailsComponent,
+    ConfirmReservationComponent
   ],
   imports: [
     BrowserModule,
