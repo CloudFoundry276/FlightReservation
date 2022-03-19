f0efdea03502b1e23548e61d1438eee518491a0c
CloudFoundry276 <cloudfoundry276@gmail.com>
2022-03-19 18:11:26 +0530

    configure routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConfirmReservationComponent } from './components/confirm-reservation/confirm-reservation.component';
+import { DisplayFlightsComponent } from './components/display-flights/display-flights.component';
+import { FindFlightsComponent } from './components/find-flights/find-flights.component';
+import { PassengerDetailsComponent } from './components/passenger-details/passenger-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '', pathMatch: 'full'},
+  {
+    path: 'findFlights',
+    component: FindFlightsComponent
+  },
+  {
+    path: 'displayFlights',
+    component: DisplayFlightsComponent
+  },
+  {
+    path: 'passengerDetails/:id',
+    component: PassengerDetailsComponent
+  },
+  {
+    path: 'confirmReservation/:id',
+    component: ConfirmReservationComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
